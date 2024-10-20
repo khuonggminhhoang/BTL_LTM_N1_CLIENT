22218d1d785553f9db5471910b61e9235cf8dded
Khuong <minhkhuong782k3@gmail.com>
2024-10-20 09:39:12 +0700

    hello world

--- src/btl_ltm_n1_client/BTL_LTM_N1_Client.java
+++ src/btl_ltm_n1_client/BTL_LTM_N1_Client.java
@@ -14,7 +14,7 @@ public class BTL_LTM_N1_Client {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        System.out.println("Hello client"); 
+   }
     
 }
